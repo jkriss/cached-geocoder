55d2ff790169acca7e97e4cb042dabcdf89b12e4
Jesse Kriss <jesse@jklabs.net>
2017-01-29 20:22:00 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const geocoder = require('geocoder')
+const level = require('level')
+
+module.exports = function(opts) {
+  if (!opts) opts = {}
+  const db = level(opts.db || './geocoder.db', { valueEncoding: 'json' })
+
+  const cachedGeocoder = function(address, cb) {
+    db.get(address.toLowerCase(), function(err, value) {
+      if (err && err.type !== 'NotFoundError') return cb(err)
+      if (value) return cb(null, JSON.parse(value))
+      geocoder.geocode(address, function(err, result) {
+        if (err) return cb(err)
+        db.put(address.toLowerCase(), JSON.stringify(result), function(err) {
+          if (err) return cb(err)
+          cb(null, result)
+        })
+      })
+    })
+  }
+
+  return {
+    geocode: cachedGeocoder,
+    selectProvider: geocoder.selectProvider
+  }
+}
+
+if (require.main === module) {
+  const cachedGeocoder = module.exports()
+  cachedGeocoder.geocode("56 Burditt Ave, Hingham, MA 02043, USA", function(err, result) {
+    console.log("final result", err, JSON.stringify(result, null, 2), typeof result)
+  })
+}
